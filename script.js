b3c5943d8047e1b6dd3f362f8f0eb6d0e59710f6
Dimitris Ververis <100343708+ververisd@users.noreply.github.com>
2026-06-03 12:45:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const card = document.getElementById('card');
 card.addEventListener('click', () => {
     card.classList.toggle('flipped');
 });
+const hint = document.querySelector('.hint');
+
+card.addEventListener('click', () => {
+    card.classList.toggle('flipped');
+    hint.style.display = 'none';
+});
